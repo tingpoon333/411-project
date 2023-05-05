2808cb6854e5b530fa472494abc5287e9ed7d73c
jdkim128 <126119581+jdkim128@users.noreply.github.com>
2023-05-05 19:29:39 -0400

    Updated Database with Final Changes

--- Database/Game_Ranker_Database.sql
+++ Database/Game_Ranker_Database.sql
@@ -1,22 +1,24 @@
 CREATE DATABASE IF NOT EXISTS gamerank;
 USE gamerank;
-DROP TABLE IF EXISTS Users CASCADE;
 DROP TABLE IF EXISTS User_Games CASCADE;
-DROP TABLE IF EXISTS Games CASCADE;
+DROP TABLE IF EXISTS recs CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id int4  NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    password varchar(255) NOT NULL,
-    birth_date varchar(11),
-    first_name varchar(40) NOT NULL,
-    last_name varchar(40) NOT NULL,
-    PRIMARY KEY (user_id)
-); 
+uid int4 NOT NULL AUTO_INCREMENT,
+email varchar(255) NOT NULL UNIQUE,
+primary key (uid)
+);
+
 CREATE TABLE User_Games (
-    user_id int4 NOT NULL,
-    game_id int4 NOT NULL AUTO_INCREMENT,
-    game_name varchar(255) NOT NULL UNIQUE,
-    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (game_id)
+game_id int4 NOT NULL AUTO_INCREMENT,
+game_name varchar(255) NOT NULL,
+date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (game_id)
+);
+
+CREATE TABLE recs (
+rid int4 NOT NULL AUTO_INCREMENT,
+rec_game_name varchar(255) NOT NULL,
+PRIMARY KEY(rid)
 );
